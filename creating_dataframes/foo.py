45ffb4a2df58f9bf1865864f774ceec1326860e2
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-04-27 11:31:34 +0530

    Script is about done and outputs to a dataframe and csv

--- creating_dataframes/foo.py
+++ creating_dataframes/foo.py
@@ -5,7 +5,9 @@ import pprint
 import collections
 
 listoffiles = os.listdir("./Data/Fights")
+
 def flatten(d, parent_key='', sep='_'):
+    print "in flatten"
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
@@ -19,29 +21,52 @@ def create_dict(indict):
   return {
       'Event_ID': indict['FMLiveFeed']['EventID'],
       'Fight_ID': indict['FMLiveFeed']['FightID'],
-      'Blue_fighter_id': indict['FMLiveFeed']['Fighters']['Blue']['FighterID'],
-      'Blue_name': indict['FMLiveFeed']['Fighters']['Blue']['Name'],
-      'Red_fighter_id': indict['FMLiveFeed']['Fighters']['Red']['FighterID'],
-      'Red_name': indict['FMLiveFeed']['Fighters']['Red']['Name']
+      'Blue_Fighter_ID': indict['FMLiveFeed']['Fighters']['Blue']['FighterID'],
+      'Blue_Name': indict['FMLiveFeed']['Fighters']['Blue']['Name'],
+      'Red_Fighter_ID': indict['FMLiveFeed']['Fighters']['Red']['FighterID'],
+      'Red_Name': indict['FMLiveFeed']['Fighters']['Red']['Name']
   }
-def readfile(inputstr, header):
-  global count
-  count +=1
-  print count
+def readfile(inputstr, header,flag=False):
+  print "in readFile"
   with open(header + inputstr) as datafile:
-    return json.load(datafile)
+    herp = json.load(datafile)
+    if(flag):
+      event_fight = inputstr.split('_')
+      herp['Event_id'] = event_fight[0]
+      herp['Fight_id'] = event_fight[1].split('.')[0]
+      boobs = herp['Fighter']
+    return herp
+
+def EditDict(indict):
+  print "in EditDict"
+  bluefighterstring = str(indict['Event_ID'])+str('_')+str(indict['Fight_ID'])+str('_')+ str(indict['Blue_Fighter_ID'])+'.json'
+  redfighterstring = str(indict['Event_ID'])+str('_')+str(indict['Fight_ID'])+str('_')+ str(indict['Red_Fighter_ID'])+'.json'
+  blue_fighter_dict = readfile(bluefighterstring,'./Data/Fights_Fighter/')
+  red_fighter_dict = readfile(redfighterstring,'./Data/Fights_Fighter/')
+  val = blue_fighter_dict['Fighter']
+  blue_fighter_dict['BlueFighter'] = val
+  val = blue_fighter_dict['Fighter_stats']
+  blue_fighter_dict['Blue_Fighter_stats'] = val
+  blue_fighter_dict.pop('Fighter',None)
+  blue_fighter_dict.pop('Fighter_stats',None)
+  indict.update(blue_fighter_dict)
+  val = red_fighter_dict['Fighter']
+  red_fighter_dict['RedFighter'] = val
+  val = red_fighter_dict['Fighter_stats']
+  red_fighter_dict['Red_Fighter_stats'] = val
+  red_fighter_dict.pop('Fighter',None)
+  red_fighter_dict.pop('Fighter_stats',None)
+  indict.update(red_fighter_dict)
+  return indict
 
 pp = pprint.PrettyPrinter(indent=2)
-count = 0
 jsons = [readfile(x,"./Data/Fights/") for x in listoffiles]
-FighterAndEventDict = map(create_dict, jsons)
-count = 0
-listoffiles = os.listdir("./Data/Fights_Fighter")
-jsons = [readfile(x,"./Data/Fights_Fighter/") for x in listoffiles]
-flattenedjsons = [flatten(x) for x in jsons]
-pp.pprint(flattenedjsons[0]) 
+FighterAndEventDic = map(create_dict, jsons)
+# At this point I have a list of dict with Event ID, Fight ID 
+# and both red and blue fighter IDs
+# Now iterate through this dict and edit the members. 
 
-
-
-df = pd.DataFrame.from_records(FighterAndEventDict)
-print df.head()
+updatedDicts = map(EditDict,FighterAndEventDic)
+flatteneddicts = map(flatten,updatedDicts)
+df = pd.DataFrame.from_records(flatteneddicts)
+df.to_csv('./Data/finalout.csv')
