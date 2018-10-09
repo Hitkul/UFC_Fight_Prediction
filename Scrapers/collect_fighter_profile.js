54d02a115462a77c34e2c4b23eab8a8bff36bf0c
Hitkul <hitkuljangid@gmail.com>
2018-10-09 19:43:04 +0530

    alll the scrapers done

--- Scrapers/collect_fighter_profile.js
+++ Scrapers/collect_fighter_profile.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var jsonfile = require('jsonfile')
 var ufc = require('ufc');
 var count =0;
-var array = fs.readFileSync('file.txt').toString().split("\n");
+var array = fs.readFileSync('data/profile_links/fighter_profile_links0.txt').toString().split("\n");
 array.forEach(function(value){
 		var url = "http://www.ufc.com"+value
 		ufc.getFighter(url, function(data) {
